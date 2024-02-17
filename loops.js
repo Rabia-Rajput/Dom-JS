4986b7974076ba889fdcdc40575b009e64f545a2
Rabia-Rajput <rajput.rabia@outlook.com>
2024-02-17 17:36:30 +0100

    for-in loop is added

--- loops.js
+++ loops.js
@@ -6,3 +6,14 @@ const users = ['Anna', 'Rabia', 'Zeeshan'];
 for (const user of users){
     console.log(user);
 }
+
+const loggedInUser = {
+    name:'Rabia',
+    age : 24,
+    isAdmin:true
+
+};
+for( const key in loggedInUser){
+    console.log(key);
+    console.log(loggedInUser[key]);
+}
