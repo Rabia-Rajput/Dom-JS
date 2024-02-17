5b0aa2561e9e86e18409b5d9743843ae5325dadd
Rabia-Rajput <rajput.rabia@outlook.com>
2024-02-17 17:42:05 +0100

    for-in loop is added

--- loops.js
+++ loops.js
@@ -17,3 +17,8 @@ for( const key in loggedInUser){
     console.log(key);
     console.log(loggedInUser[key]);
 }
+let isFinished = false;
+while(!isFinished){
+    isFinished = confirm('Do you want to quit?');
+}
+console.log('Done!');
